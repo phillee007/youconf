f9c6e16745adae641e179a2584bcaf93010a2430
Phil Lee <phil.lee.007@gmail.com>
2013-05-08 10:07:37 +1200

    Minor update to stop CSS breaking minifier

--- YouConf/App_Start/BundleConfig.cs
+++ YouConf/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace YouConf
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
