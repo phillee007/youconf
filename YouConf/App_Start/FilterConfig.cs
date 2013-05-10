d5544543df7fbfb2d603150668b9fa77ba439aac
Phil Lee <phil.lee.007@gmail.com>
2013-05-10 21:31:05 +1200

    Fixed up issue with error logging

--- YouConf/App_Start/FilterConfig.cs
+++ YouConf/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace YouConf
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+           // filters.Add(new HandleErrorAttribute());
         }
     }
 }
---
+++ YouConf/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YouConf.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult ServerError()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+    }
+}
