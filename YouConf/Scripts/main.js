d3b41cbdc1172c23b305e66732dcd6875613cbfc
Phil Lee <phil.lee.007@gmail.com>
2013-06-23 21:45:38 +1200

    Fixed bug which was adding separator when it shouldn't

--- YouConf/Scripts/main.js
+++ YouConf/Scripts/main.js
@@ -13,7 +13,7 @@
 
     //Add pretty dividers to the relevant articles. We could use a CSS :last-child attribute for this, but it will not work in as many browsers
     $("<hr class='separator' />")
-        .insertAfter("#main-content .box article:not(:last)");
+        .insertAfter("#main-content .box article:not(:last-of-type)");
 
     //HACK - Wait 3 seconds till showing the social buttons...
     //They seem to take an age to load, so why not hide them till they're all visible (hopefully 3 seconds is enough)
